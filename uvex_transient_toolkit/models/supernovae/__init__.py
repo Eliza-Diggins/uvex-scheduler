1e17212cf83ce0572f832bc0cd546231eaad0e0f
Eliza Diggins <eliza.diggins@utah.edu>
2026-09-03 08:35:38 -0700

    chore: removed old top-hat CCSNe.

--- uvex_transient_toolkit/models/supernovae/__init__.py
+++ uvex_transient_toolkit/models/supernovae/__init__.py
@@ -3,12 +3,10 @@ Composite supernova SED models.
 """
 
 __all__ = [
-    "TopHatCCSNeSED",
     "TypeIIPExcessSED",
     "TypeIIPSED",
     "VillarCoolingBlackbodySED",
 ]
 
 from ._IIp_excess import TypeIIPExcessSED, TypeIIPSED
-from ._tophat import TopHatCCSNeSED
 from ._villar import VillarCoolingBlackbodySED
